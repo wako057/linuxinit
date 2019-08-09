caed4a35aea124b18452bbf30219c207efc24551
Gregory Gerard <wako057@gmail.com>
2019-08-09 09:27:57 +0000

    Update init-v2.sh

--- init-v2.sh
+++ init-v2.sh
@@ -3,7 +3,21 @@
 RED="\e[31m"
 #GREEN="\e[32m"
 CYAN="\e[36m"
-
+BLACK=$(tput setaf 0)
+RED=$(tput setaf 1)
+GREEN=$(tput setaf 2)
+YELLOW=$(tput setaf 3)
+LIME_YELLOW=$(tput setaf 190)
+POWDER_BLUE=$(tput setaf 153)
+BLUE=$(tput setaf 4)
+MAGENTA=$(tput setaf 5)
+CYAN=$(tput setaf 6)
+WHITE=$(tput setaf 7)
+BRIGHT=$(tput bold)
+NORMAL=$(tput sgr0)
+BLINK=$(tput blink)
+REVERSE=$(tput smso)
+UNDERLINE=$(tput smul)
 
 log () {
     # Display log messages
